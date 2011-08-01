924a852828c5d231fe87100ca4431d55f02e6d13
psvr <psvr@localhost.localdomain>
2011-08-01 23:34:24 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intjuice::Application.config.secret_token = 'e79a780377ee2c6e39439b15678716d44d447a7daec67e6391bd907d4dba30131d5e7f199bc01f07443054de33e6cd07b606a52f4d01905895e9db4f6a63b183'
